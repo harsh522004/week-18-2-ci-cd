baacdf6f34ec31e44c59feb48a55d4b7a3dc2726
Harsh Butani <harshbutani2004@gmail.com>
2024-07-11 13:03:01 +0530

    some

--- apps/user-app/app/api/auth/[...nextauth]/route.ts
+++ apps/user-app/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import { authOptions } from "../../../lib/auth";
 
 console.log("hey harsh");
+console.log("hey harsh");
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
